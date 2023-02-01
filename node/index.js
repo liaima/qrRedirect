d1b6d5ac3bbf5a1351bdcf70acde749b0557bf0e
Manuel Lopez Jove <liaimasoft@gmail.com>
2023-02-01 17:20:10 -0300

    models

--- node/index.js
+++ node/index.js
@@ -1,6 +1,10 @@
 import app from './app.js';
 import db from './config/db.js';
 
+import './models/User.js';
+import './models/Qr.js';
+import './models/Scan.js';
+
 const PORT = process.env.NODE_PORT || 8000;
 
 const main = async () => {
